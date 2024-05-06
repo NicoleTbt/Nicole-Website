3fd23d92991820f4fb67b794078a8fa04e85bf77
NicoleTbt <124452338+NicoleTbt@users.noreply.github.com>
2024-05-07 01:05:00 +0300

    Initial commit with Firebase Hosting setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Nicole-Website/"
-})
+  base: "/Nicole-Website/",
+  build: {
+    rollupOptions: {
+      external: ['firebase/firestore'], // Add this line
+    },
+  },
+});
