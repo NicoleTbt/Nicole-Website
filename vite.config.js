d4f64d94ed6738e0d46b9cfa7fd8762183d8608a
NicoleTbt <124452338+NicoleTbt@users.noreply.github.com>
2024-05-07 01:28:09 +0300

    Final Try

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Nicole-Website/",
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/firestore', 'firebase/storage', '@firebase/firestore'],
-    },
-  },
-});
+  base: "/Nicole-Website/"
+})
